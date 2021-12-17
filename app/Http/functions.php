88ff07b9ef9d51106243c52d91cae16434a0a436
Nadali Khalili <nadalikh@gmai.com>
2021-12-17 16:11:16 +0330

    first test on quize on direcotr

--- app/Http/functions.php
+++ app/Http/functions.php
@@ -7,10 +7,11 @@ class functions
     static function directorQuiz($movie)
     {
         $answers = array();
-        $answers[] = $movie;
+
         $answers[] = $movie::where('director', "!=", $movie->director)->first();
         $answers[] = $movie::where('director', "!=", $answers[0]->director)->first();
         $answers[] = $movie::where('director', "!=", $answers[1]->director)->first();
+        $answers[] = $movie;
         shuffle($answers);
         return $answers;
     }
