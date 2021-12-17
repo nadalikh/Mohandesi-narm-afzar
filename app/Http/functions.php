10f3470e0c29ee1ca27a0ab331e0fa411b4ee014
Nadali Khalili <nadalikh@gmai.com>
2021-12-17 16:50:15 +0330

    first test on quize on direcotr

--- app/Http/functions.php
+++ app/Http/functions.php
@@ -9,8 +9,8 @@ class functions
         $answers = array();
 
         $answers[] = $movie::where('director', "!=", $movie->director)->inRandomOrder()->first();
-        $answers[] = $movie::where(['director', "!=", $answers[0]->director],['director', "!=", $movie->director])->inRandomOrder()->first();
-        $answers[] = $movie::where(['director', "!=", $answers[1]->director],['director', "!=", $answers[0]->director],['director', "!=", $movie->director])->inRandomOrder()->first();
+        $answers[] = $movie::where([['director', "!=", $answers[0]->director],['director', "!=", $movie->director]])->inRandomOrder()->first();
+        $answers[] = $movie::where([['director', "!=", $answers[1]->director],['director', "!=", $answers[0]->director],['director', "!=", $movie->director]])->inRandomOrder()->first();
         $answers[] = $movie;
         shuffle($answers);
         return $answers;
