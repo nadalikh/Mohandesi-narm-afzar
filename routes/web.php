3898d4343d13f7bc005da3abb68460daf1081795
Nadali Khalili <nadalikh@gmai.com>
2021-12-20 22:08:43 +0330

    test on getting answer

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/director', [\App\Http\Controllers\HomeController::class, "answerValidation"])->name("directorAns");
+Route::get('/director', [App\Http\Controllers\HomeController::class, "answerValidation"])->name("directorAns");
 
 Auth::routes();
 
