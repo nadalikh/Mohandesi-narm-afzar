94f68d15ac574066fae001a1a52a0cf428fdf28a
Nadali Khalili <nadalikh@gmai.com>
2021-12-25 20:23:41 +0330

    test on getting answer

--- app/Http/functions.php
+++ app/Http/functions.php
@@ -8,8 +8,8 @@ class functions
 {
      static function directorQuestion()
     {
-        $quiz["question"] = movie::all()->random(1)->first();
         $answers = $quiz = array();
+        $quiz["question"] = movie::all()->random(1)->first();
         $answers[] = $quiz["question"]::where('director', "!=", $quiz["question"]->director)->inRandomOrder()->first();
         $answers[] = $quiz["question"]::where([['director', "!=", $answers[0]->director],['director', "!=", $quiz["question"]->director]])->inRandomOrder()->first();
         $answers[] = $quiz["question"]::where([['director', "!=", $answers[1]->director],['director', "!=", $answers[0]->director],['director', "!=", $quiz["question"]->director]])->inRandomOrder()->first();
