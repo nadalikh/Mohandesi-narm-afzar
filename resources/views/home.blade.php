5424bf3999c6a9837c0df75a903e26543f48834f
Nadali Khalili <nadalikh@gmai.com>
2021-12-30 13:50:02 +0330

    answering question

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header border-light p-3 bg-dark text-light">quiz</div>
+                <div class="card-header border-light p-3 bg-dark text-light">Quiz</div>
 
                 @isset($status)
                     @if($status)
