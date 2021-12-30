ab60e23d234527853dd719314c5547e4d40b4046
Nadali Khalili <nadalikh@gmai.com>
2021-12-30 13:47:59 +0330

    answering question

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header bg-dark text-light">quiz</div>
+                <div class="card-header border-light p-3 bg-dark text-light">quiz</div>
 
                 @isset($status)
                     @if($status)
@@ -15,7 +15,7 @@
                     @endif
 
                 @endisset
-                <form method="get" action="{{route("directorAns")}}" class="form-check bg-dark">
+                <form method="get" action="{{route("directorAns")}}" class="form-check bg-dark p-3">
                     <p class="text-light">Who is the director of {{$quiz["question"]->title}} movie which was published in {{$quiz["question"]->year}} in {{$quiz["question"]->country}}?</p>
                     <input type="hidden" name="movieId" value="{{$quiz["question"]->imdb_title_id}}">
                     <div class="row justify-content-around">
